3ff6ed8771abf444f06da3d7861fe1cf5d2713eb
vTarlettskiy <v.tarlettskiy@gmail.com>
2024-01-07 19:52:58 +0300

    02-js-hometask, even or odd

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,11 @@
 // code
+
+evenOrOdd();
+function evenOrOdd() {
+    let number1 = Math.floor(Math.random() * 100)
+    if (number1 % 2 === 0) {
+        console.log(`${number1} :Это четное число`)
+    } else {
+        console.log(`${number1} :Это нетное число`)
+    }
+}
