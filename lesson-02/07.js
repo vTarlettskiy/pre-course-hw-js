f7abef40a6f82c014a715c46ac8c9b98b186270d
vTarlettskiy <v.tarlettskiy@gmail.com>
2024-01-14 19:11:36 +0300

    02.07-js-hometask

---
+++ lesson-02/07.js
@@ -0,0 +1,15 @@
+let passportMarried = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "Bobryisk"
+    }
+};
+
+let passportMarried2 = structuredClone(passportMarried);
+
+passportMarried2.married = true;
+
+console.log(passportMarried);
+console.log(passportMarried2);
